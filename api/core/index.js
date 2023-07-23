aff0148b56a6482064d90c8de7839718afc56f29
Lucas Boemeke <lucas@blueshift.cc>
2023-07-23 05:25:35 -0300

    fix api

--- api/core/index.js
+++ api/core/index.js
@@ -1,6 +1,7 @@
 const api = require('lambda-api')();
 const createClient = require('@libsql/client').createClient;
 const client = createClient({ url: 'libsql://gpthunters-boemekeld.turso.io', authToken: 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTAwOTUzMDIsImlkIjoiYjM1YjEwNjctMjkyNS0xMWVlLTk1ODEtMWE4NjY5MWUyODU2In0.T93iddRYFyfM0hKZo0zxaM131V8TbtZN80uC8NWlNSJz7EHhW-pMptnAlH6ZcIAZlaocfc_PRrFbjnXgdW99Dw' });
+const { v4: uuidv4 } = require('uuid');
 
 api.get('/', async (req, res) => {
   return { status: 'ok' };
@@ -22,6 +23,55 @@ api.get('/list', async (req, res) => {
   }
 });
 
+api.get('/table', async (req, res) => {
+  try {
+    let rs = null;
+    rs = await client.execute({
+      sql: `
+        DROP TABLE chat;
+      `,
+      args: []
+    });
+    console.log(rs);
+    rs = await client.execute({
+      sql: `
+        CREATE TABLE chat (
+          id TEXT PRIMARY KEY,
+          title TEXT,
+          url TEXT,
+          author TEXT,
+          date TEXT,
+          content TEXT,
+          votes INTEGER
+        );
+      `,
+      args: []
+    });
+    console.log(rs);
+    /*
+    rs = await client.execute({
+      sql: "INSERT INTO chat VALUES(?,?,?,?,?,?,?)",
+      args: [
+        uuidv4(),
+        'title',
+        'url',
+        'author',
+        'date',
+        'content',
+        1
+      ]
+    });
+    console.log(rs);
+    */
+    return { status: 'ok' };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error.message),
+    };
+  }
+});
 
 api.get('/crawler', async (req, res) => {
   try {
@@ -54,13 +104,13 @@ api.get('/crawler', async (req, res) => {
     await client.execute({
       sql: "INSERT INTO chat VALUES(?,?,?,?,?,?,?)",
       args: [
-        Math.random(),
+        uuidv4(),
+        data.title,
         url,
         data.author,
         data.date,
         data.content,
-        '0',
-        'create_at'
+        1
       ]
     });
 
