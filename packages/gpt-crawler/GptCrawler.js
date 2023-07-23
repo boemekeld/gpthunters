0ceeec8d0dfd86e87cd30422798e142c29c53f57
Lucas Boemeke <lucas@blueshift.cc>
2023-07-23 01:16:39 -0300

    .

---
+++ packages/gpt-crawler/GptCrawler.js
@@ -0,0 +1,44 @@
+import { JSDOM } from 'jsdom';
+import fs from 'fs';
+
+class GptCrawler {
+  async fetchUrl(url) {
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.text();
+
+      const dom = new JSDOM(data);
+      const document = dom.window.document;
+
+      const title = document.querySelector('h1').textContent;
+      let author = null;
+      let date = null;
+      
+      if (document.querySelector('.pt-3.text-base.text-gray-400 span')) {
+        author = document.querySelector('.pt-3.text-base.text-gray-400 span').textContent;
+        date = document.querySelector('.pt-3.text-base.text-gray-400').childNodes[2].textContent;
+      } else {
+        date = document.querySelector('.pt-3.text-base.text-gray-400').textContent;
+      }
+
+      const content = Array.from(document.querySelectorAll('.flex.flex-col.h-full.text-sm .group')).map(item => item.innerHTML);
+      fs.writeFileSync('content.html', content.join("\n\n\n\n"));
+
+      return {
+        title,
+        author,
+        date,
+        content
+      };
+    } catch (error) {
+      console.error(`There was a problem with the fetch operation: ${error.message}`);
+    }
+  }
+}
+
+export default GptCrawler;
