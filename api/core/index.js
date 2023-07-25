13561e251972076da0053450187a4938e806973f
Matheus Oliveira <matheusvp2@ucl.br>
2023-07-25 01:31:27 -0300

    Restaurando codigo

--- api/core/index.js
+++ api/core/index.js
@@ -2,12 +2,14 @@ const createClient = require('@libsql/client').createClient;
 const client = createClient({ url: 'libsql://gpthunters-boemekeld.turso.io', authToken: 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTAwOTUzMDIsImlkIjoiYjM1YjEwNjctMjkyNS0xMWVlLTk1ODEtMWE4NjY5MWUyODU2In0.T93iddRYFyfM0hKZo0zxaM131V8TbtZN80uC8NWlNSJz7EHhW-pMptnAlH6ZcIAZlaocfc_PRrFbjnXgdW99Dw' });
 const { v4: uuidv4 } = require('uuid');
 
+
 const lambda = require('lambda-api')
 
 const api = lambda({
   cors: true,
   corsAllowOrigin: '*',
 });
+
 
 api.get('/', async (req, res) => {
   return { status: 'ok' };
