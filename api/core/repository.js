7935836ca64dad8e40b7f99edaa394f717a98c32
Matheus Oliveira <matheusvp2@ucl.br>
2023-07-24 22:12:50 -0300

    Testando repository

--- api/core/repository.js
+++ api/core/repository.js
@@ -1,12 +1,7 @@
 const { createClient } = require("@libsql/client");
 const { v4: uuidv4 } = require('uuid');
-const { sqlClient } = require('./config');
 
 class ChatRepository {
-
-    constructor() {
-        this.client = createClient(sqlClient);
-    }
 
     async findAll(chat) {
         return "String"
