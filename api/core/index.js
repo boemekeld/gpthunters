dd61d279d8e0da9d8a2d11c6e07a795b15aa633a
Matheus Oliveira <matheusvp2@ucl.br>
2023-07-24 21:28:23 -0300

    Testando organizacao de repositorio

--- api/core/index.js
+++ api/core/index.js
@@ -3,14 +3,14 @@ const createClient = require('@libsql/client').createClient;
 const client = createClient({ url: 'libsql://gpthunters-boemekeld.turso.io', authToken: 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTAwOTUzMDIsImlkIjoiYjM1YjEwNjctMjkyNS0xMWVlLTk1ODEtMWE4NjY5MWUyODU2In0.T93iddRYFyfM0hKZo0zxaM131V8TbtZN80uC8NWlNSJz7EHhW-pMptnAlH6ZcIAZlaocfc_PRrFbjnXgdW99Dw' });
 const { v4: uuidv4 } = require('uuid');
 
-const { xpto } = require('./repository')
+const { ChatRepository } = require('./repository');
+
+const chatRepository = new ChatRepository();
+
+
 
 api.get('/', async (req, res) => {
-  return { status: 'ok', message: xpto() };
-});
-
-api.get('/testando', async (req, res) => {
-  return { status: 'ok', message: xpto() };
+  return { status: 'ok', all: chatRepository.findAll() };
 });
 
 api.get('/list', async (req, res) => {
--- api/core/repository.js
+++ api/core/repository.js
@@ -1,7 +1,20 @@
-function xpto() {
-    return "String XPTO"
+const { createClient } = require("@libsql/client");
+const { v4: uuidv4 } = require('uuid');
+const { sqlClient } = require('./config');
+
+class ChatRepository {
+
+    constructor() {
+        this.client = createClient(sqlClient);
+    }
+
+    async findAll(chat) {
+        return "String"
+    }
+
 }
 
+
 module.exports = {
-    xpto
+    ChatRepository
 }
